3bc660bee34c5fa56e01e489673fc7d7d1000c7e
nimish <nimish4july1998@gmail.com>
2018-06-11 21:35:36 +0530

    succesfully integrated media

--- ecommercewebsite/products/models.py
+++ ecommercewebsite/products/models.py
@@ -8,10 +8,15 @@ def product_directory_path(instance, filename):
 
 
 class Product(models.Model):
+    id=models.IntegerField(primary_key=True)
     title=models.CharField(max_length=120)
     description=models.TextField()
     price=models.DecimalField(max_digits=20,decimal_places=2)
-    image=models.FileField(upload_to=product_directory_path,blank=True,null=True)
+    image=models.ImageField(upload_to=product_directory_path,blank=True,null=True)
+
+    def get_image_url(self):
+        print("getting url")
+        return self.image.url
 
     def __str__(self):
         return self.title
--- ecommercewebsite/products/views.py
+++ ecommercewebsite/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 # Create your views here.
+from .models import Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_details.html"
+
+
+    def get_object(self, queryset=None):
+        print("looking pk ",self.kwargs)
+        return get_object_or_404(Product,id=self.kwargs.get("pk"))
+
+    # def get_context_data(self, **kwargs):
+    #     print("hello")
+    #     context = super(ProductDetailView, self).get_context_data(**kwargs)
+    #     context['object'] = Product.objects.filter(pk=self.kwargs.get("pk"))
+    #     return context
+
+
